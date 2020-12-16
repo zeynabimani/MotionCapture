75f27c4c43fe31a805ad17c4c6776d522cfeef11
zeynabimani <zeynab.s.imani@gmail.com>
2020-12-16 18:23:10 +0330

    codes should be written in the "command view" on Matlab

---
+++ webcam.m
@@ -0,0 +1,18 @@
+% webcamlist
+
+%%turn on the camera
+% cam2 = webcam(2)
+% cam3 = webcam(3)
+
+%%preview
+% preview(cam2)
+% preview(cam3)
+
+%%close preview
+% closePreview(cam2)
+% closePreview(cam3)
+
+%%turn off the camera
+% clear(cam2)
+% clear(cam3)
+
