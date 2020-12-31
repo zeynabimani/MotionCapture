5b94c53a3a79306fad0613e400c049c8e920b892
fbghkh1999 <40722872+fbghkh1999@users.noreply.github.com>
2021-01-01 01:52:42 +0330

    add logFile

--- SortMatches.m
+++ SortMatches.m
@@ -3,7 +3,9 @@ function Center2=SortMatches(maches,center2)
 
 for i=1:(length(maches))
 if maches(i)~=i
-    Center2(i,:)=center2(maches(i),:);   
+    Center2(i,:)=center2(maches(i),:);
+else
+    Center2(i,:)=center2(i,:);
 end
-  disp(Center2);
+  
 end
