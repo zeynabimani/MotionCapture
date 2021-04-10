fa0f06a8317df6a594f62b05350f190c0d46dfe4
fbghkh1999 <40722872+fbghkh1999@users.noreply.github.com>
2021-04-11 01:33:43 +0430

    add openpose 3d function fix bug

--- openpose3d.m
+++ openpose3d.m
@@ -5,27 +5,27 @@ str1=['matfiles/center1/data1_' num2str(i-1) '.mat'];
 
 load(str1)
 size(arr)
-if i==1
- for o=1:3:75
-    
-    
-   figure(1)
-   plot(arr(1,o),arr(1,o+1),'o')
-   hold on
- end
-end
-% center1=zeros(75,2);
-% for n=1:75
-%   for m=1:2
-% center1(n,m)=arr(1,2);
-%   end
-% end
 
-% str2=['matfiles/center2/data2_' num2str(i-1) '.mat'];
-% load(str2)
-% center2=arr;
-% load('results/calibrationSession6.mat');
-% point3d = triangulate(center1,center2,calibrationSession.CameraParameters);
-% point3d
+center1=zeros(25,2);
+    b=1
+    for n=1:3:75
+    center1(b,1)=arr(1,n);
+    center1(b,2)=arr(1,n+1);
+    b=b+1;
+    end
+center1
+str2=['matfiles/center2/data2_' num2str(i-1) '.mat'];
+load(str2)
+center2=zeros(25,2);
+ w=1;
+    for n=1:3:75
+    center2(w,1)=arr(1,n);
+    center2(w,2)=arr(1,n+1);
+    w=w+1;
+    end
+center2
+load('results/calibrationSession6.mat');
+point3d = triangulate(center1,center2,calibrationSession.CameraParameters);
+point3d
 end
  
